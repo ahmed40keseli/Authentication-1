1c430b93ef86c68d70a22038a18b792b8186a83b
ahmed40keseli <krkkseli@gmail.com>
2024-01-29 18:44:04 +0300

    arrangement

--- controllers/login.js
+++ controllers/login.js
@@ -11,8 +11,13 @@ const login = async(req,res) =>{
             if (!result[0]  || !await bcrypt.compare(password, result[0].password))return res.json({status:"error", error:"plase enter your email and password"})
             else{
                 const token = jwt.sign({id:result[0].id},process.env.JWT_SECRET,{
-                    expiresIn:process.env.JWT_EXPIRES
+                    expiresIn:process.env.JWT_EXPIRES,
+                    httpOnly:true
                 })
+                const cookieOptions = {
+                    expiresIn: new Date(date.now()+process.env.COOKIE_EXPIRS + 24 * 60 * 60 * 1000),
+                    httpOnly:true
+                }
             }
         })
     }
