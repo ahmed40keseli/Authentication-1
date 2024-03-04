3ccc089eb68d24d0732ffef0d4d9bd7d86d61b58
ahmed40keseli <krkkseli@gmail.com>
2024-03-04 18:02:44 +0300

    debug-2

--- controllers/register.js
+++ controllers/register.js
@@ -1,25 +1,36 @@
 const db = require('../routes/db-config');/*Önceden yapılandırılmış veritabanı bağlantısını modülden içe aktarır (db-config) */
 const bcrypt = require('bcryptjs');/* Parolaları güvenli bir şekilde karma hale getirmek için kullanılan kitaplığı içe aktarır.*/
 
-const register = async (req,res) =>{
-    const {email, password:Npassword} = req.body
-    if (!email || !Npassword) return res.json({status:"error", error:"plase enter your email and password"}); /*Hem e-postanın hem de şifrenin sağlanıp sağlanmadığını kontrol eder. 
+const register = async (req, res) => {
+    const { email, password: Npassword } = req.body
+    if (!email || !Npassword) return res.json({ status: "error", error: "plase enter your email and password" }); /*Hem e-postanın hem de şifrenin sağlanıp sağlanmadığını kontrol eder. 
                                                                                                                 Değilse, hata mesajı içeren bir JSON yanıtı döndürür. */
-    else{
+    else {
+        console.log("email");
         console.log(email);
-        db.query('SELECT email FROM users WHERE email = ?',{email},async (err,result)=>{/* veri tabanından istenilen veriyi çeker */
-            if (err) throw err;/* */
-            if (result[0]) return res.json({status:"error",error:"email has already been registered"})/* Sağlanan e-postanın zaten kayıtlı olup olmadığını kontrol etmek için bir veritabanı sorgusu gerçekleştirir. 
-                                                                                                         Eğer öyleyse, bir hata yanıtı döndürür.*/
-            else{
-                const password =await bcrypt.hash(Npassword,8);/*E-posta önceden kayıtlı değilse, bcryptsağlanan şifreyi karma hale getirmek için kullanılır. */
+        db.query('SELECT email FROM users WHERE email = ?', [email], async (err, result) => {
+            console.log("where sorgusu bitt");
+            if (err) throw err;
+        
+            console.log("result");
+            console.log(result);
+        
+            if (result.length > 0) {
+                console.log("if'e girdi");
+                return res.json({ status: "error", error: "email has already been registered" });
+            } else {
+                console.log("else'e girdi");
+                console.log(Npassword);
+                const password = await bcrypt.hash(Npassword, 8); // şifreyi şifreleyen kod
+        
                 console.log(password);
-                db.query('INSERT INTO users SET ?',{email: email,password: password},(error,result) => {/* Kullanıcının e-postasını ve karma şifresini veritabanına ekler.*/
-                    if (error) throw error;/* */
-                    return res.json({status:"success",success:"user has been registered"})/*Kayıt işleminin başarısını gösteren bir JSON yanıtı döndürür. */
-                })
-            } 
-        })
+                db.query('INSERT INTO users SET ?', { email: email, password: password }, (error, result) => {
+                    if (error) throw error;
+                    return res.json({ status: "success", success: "user has been registered" });
+                });
+            }
+        });
+        
     }
 }
 
